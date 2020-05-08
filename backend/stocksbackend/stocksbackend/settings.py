41960e5cb641808ca06ef92a1dc3006266304ada
JonasRoo <jonas.roosen@outlook.com>
2020-05-09 00:00:39 +0200

    migrated database to PostgreSQL | see settings.py on address details

--- backend/stocksbackend/stocksbackend/settings.py
+++ backend/stocksbackend/stocksbackend/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'stocksbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stockmarketapp',
+        'USER': 'postgres',
+        'PASSWORD': 'stonks123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
