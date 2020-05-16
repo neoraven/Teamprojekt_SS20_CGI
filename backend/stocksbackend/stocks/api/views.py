928a5d6652e7378d18d46a3696a15b1d62dfdf49
JonasRoo <jonas.roosen@outlook.com>
2020-05-16 16:56:38 +0200

    PEP8 Formatting

--- backend/stocksbackend/stocks/api/views.py
+++ backend/stocksbackend/stocks/api/views.py
@@ -3,18 +3,19 @@ from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from stocks.models import Stock
 from .serializers import StocksSerializer
 
-class StockRudView(generics.RetrieveUpdateDestroyAPIView):
-	permission_classes = [IsAuthenticatedOrReadOnly]
-	lookup_field = 'symbol'
-	serializer_class = StocksSerializer
 
-	def get_queryset(self):
-		return Stock.objects.all()
+class StockRudView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    lookup_field = 'symbol'
+    serializer_class = StocksSerializer
+
+    def get_queryset(self):
+        return Stock.objects.all()
 
 
 class StockAllView(generics.ListAPIView):
-	permission_classes = [AllowAny]
-	serializer_class = StocksSerializer
+    permission_classes = [AllowAny]
+    serializer_class = StocksSerializer
 
-	def get_queryset(self):
-		return Stock.objects.all()
+    def get_queryset(self):
+        return Stock.objects.all()
