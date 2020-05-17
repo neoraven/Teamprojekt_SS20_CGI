60e530b9010f0b81be35157e837cb17b4a3d1e98
JonasRoo <jonas.roosen@outlook.com>
2020-05-18 00:34:31 +0200

    Implemented Transaction model

--- backend/stocksbackend/portfolio/admin.py
+++ backend/stocksbackend/portfolio/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Portfolio
+from .models import Portfolio, Transaction
 
 admin.site.register(Portfolio)
+admin.site.register(Transaction)
--- backend/stocksbackend/portfolio/models.py
+++ backend/stocksbackend/portfolio/models.py
@@ -5,26 +5,60 @@ from django.core.validators import ValidationError
 
 
 class Portfolio(models.Model):
-	user			= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	symbol			= models.ForeignKey(Stock, on_delete=models.CASCADE)
-	amount			= models.PositiveIntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    symbol = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
 
-	def __str__(self):
-		return f'{self.user} > {self.amount} of {self.symbol.symbol}'
+    def __str__(self):
+        return f"{self.user} > {self.amount} of {self.symbol.symbol}"
 
-	def clean(self, *args, **kwargs):
-		if self.amount <= 0:
-			raise ValidationError('Amount in portfolio needs to be >0!')
-		super(Portfolio, self).clean(*args, **kwargs)
-
-	def save(self, *args, **kwargs):
-		self.full_clean()
-		super(Portfolio, self).save(*args, **kwargs)
-
-	class Meta:
-		unique_together = (('user', 'symbol'),)
+    class Meta:
+        unique_together = (("user", "symbol"),)
 
 
 class Transaction(models.Model):
-	# redundancy
-	pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    symbol = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    date_posted = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        purchase_type = "sold" if self.amount < 0 else "bought"
+        return (
+            f"{self.user} {purchase_type} {abs(self.amount)} of {self.symbol} at "
+            f"{self.date_posted.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
+
+    def clean(self, *args, **kwargs):
+        created = self.pk is None
+        if not created:
+            raise ValidationError(
+                f"Instances of the ``{self.__class__.__name__}`` model cannot be altered!"
+            )
+        if self.amount == 0:
+            raise ValidationError(
+                "Instances of Transaction cannot have an amount of 0!"
+            )
+
+    def save(self, *args, **kwargs):
+        super(Transaction, self).save(*args, **kwargs)
+        try:
+            portfolio = Portfolio.objects.get(user=self.user, symbol=self.symbol)
+            portfolio.amount += self.amount
+        except Portfolio.DoesNotExist:
+            portfolio = Portfolio(
+                user=self.user, symbol=self.symbol, amount=self.amount
+            )
+
+        if portfolio.amount < 0:
+            # a negative amount is invalid
+            raise ValidationError(
+                f"You do not have enough stocks of {self.symbol} to perform this action "
+                f"(missing amount: {abs(portfolio.amount)})."
+            )
+        elif portfolio.amount == 0:
+            # The user sold all his stocks > delete the portfolio
+            portfolio.delete()
+        else:
+            # Positive amount: save the model
+            portfolio.save()
