c79f4b91c665eb635875c42f57569e63b95c4c82
Emre Gezer <e.gezer@tu-bs.de>
2020-05-22 17:03:16 +0200

    models.py: removed date_added from 'Stock' relation data_util.py: Initial load of data + daily update possible now
    note: specify your username and keys in data_utils.py

--- backend/stocksbackend/stocks/data_util.py
+++ backend/stocksbackend/stocks/data_util.py
@@ -1,52 +1,69 @@
+import re
 import time
+from itertools import cycle
 
-from datetime import datetime
 import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 from django.contrib.auth.models import User
 
 from .models import Price, Stock
 
-USERNAME = 'emre'
+# SPECIFY THE KEYS
+# Two api keys because of the limited amount of API Calls per day (500). Stocks# in S&P 500: 505
 
-KEY = '1077F37TLBGNSVY2'
+KEY_LIST = cycle(['1077F37TLBGNSVY2', 'H73WEF95O0IAME5U'])
+
 OUTPUT_FORMAT = 'pandas'
 OUTPUT_SIZE = 'full'
 
+USERNAME = 'emre'
 user = User.objects.filter(username=USERNAME).first()
 
 
-# Create your views here.
-
-def initial_load():
+# TODO: Check wikipedia list for updates automatically, eventually: ask before adding symbol (manual check)
+def load():
     payload = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     df = payload[0]
 
     company_symbols = df['Symbol'].values.tolist()
+
+    for symbol in company_symbols:
+        pattern = re.compile('[\W_]+')
+        company_symbols[company_symbols.index(symbol)] = pattern.sub('', symbol)
+
     company_names = df['Security'].values.tolist()
 
     print("Symbols and names loaded")
     print("-------------------------------------")
 
-    ts = TimeSeries(key=KEY, output_format=OUTPUT_FORMAT)
+    ts = TimeSeries(key=next(KEY_LIST), output_format=OUTPUT_FORMAT)
 
-    for i in range(len(company_symbols)):
+    for i in range(2):
+        print(f'save {company_symbols[i]} instances...')
         data, meta_data = ts.get_daily(symbol=company_symbols[i], outputsize='full')
         date = data.index.tolist()
+        Stock(symbol=company_symbols[i],
+              company_name=company_names[i],
+              ).save()
         for j in range(len(date)):
-            Stock(symbol=company_symbols[i],
-                  company_name=company_names[i],
-                  creator=user,
-                  date_added=datetime.now()).save()
-            Price(symbol=Stock.objects.get(symbol=company_symbols[i]),
-                  interval="1d",
-                  date=date[j].date(),
-                  p_low=data['3. low'][j],
-                  p_open=data['1. open'][j],
-                  p_high=data['2. high'][j],
-                  p_close=data['4. close'][j],
-                  volume=data['5. volume'][j]
-                  ).save()
+            if Price.objects.filter(
+                    symbol=Stock.objects.get(symbol=company_symbols[i]),
+                    date=date[j].date()
+            ).exists():
+                continue
+            else:
+                Price(symbol=Stock.objects.get(symbol=company_symbols[i]),
+                      interval="1d",
+                      date=date[j].date(),
+                      p_low=data['3. low'][j],
+                      p_open=data['1. open'][j],
+                      p_high=data['2. high'][j],
+                      p_close=data['4. close'][j],
+                      volume=data['5. volume'][j]
+                      ).save()
         print(f"{company_symbols[i]} instances saved")
         print(f"-------------------")
-        time.sleep(3)
+        time.sleep(2)
+
+        ts = TimeSeries(key=next(KEY_LIST), output_format=OUTPUT_FORMAT)
+
--- backend/stocksbackend/stocks/models.py
+++ backend/stocksbackend/stocks/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Stock(models.Model):
     symbol = models.CharField(primary_key=True, max_length=5)
     company_name = models.TextField(max_length=50, null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.symbol
