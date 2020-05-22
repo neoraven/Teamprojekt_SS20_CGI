7ea9463b3c58deb63d2aab2e04be310e6d1c6ecd
JonasRoo <jonas.roosen@outlook.com>
2020-05-22 13:58:20 +0200

    Updated docstrings

--- backend/stocksbackend/stocks/api/views.py
+++ backend/stocksbackend/stocks/api/views.py
@@ -11,6 +11,16 @@ from .serializers import StocksSerializer
 
 
 class StockRudView(generics.RetrieveUpdateDestroyAPIView):
+    """READ / PUT / PATCH / DELETE endpoint for stocks.
+
+    Permissions:
+        IsAdminUser -- for PUT / PATCH / DELETE
+    
+    Request data:
+        symbol {str} -- from URI. Stock symbol to look up in database.
+        company_name {str} (optional) -- for PUT / PATCH / DELETE.
+    """
+
     permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
     lookup_field = "symbol"
     serializer_class = StocksSerializer
@@ -21,12 +31,37 @@ class StockRudView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class StockCreateView(generics.CreateAPIView):
+    """POST endpoint to create new stocks.
+    
+    Permissions:
+        IsAdminUser
+    
+    Request data:
+        symbol {str} -- Stock symbol (max_length=5)
+        company_name {str} -- Name of company for given symbol (max_length=5 | nullable)  
+    """
+
     queryset = Stock.objects.all()
     permission_classes = [IsAdminUser]
     serializer_class = StocksSerializer
 
 
 class StockAllView(generics.ListAPIView):
+    """GET endpoint to get a list of all stock objects.
+
+    Permissions:
+        AllowAny
+    
+    Request data:
+        None
+    
+    Response data:
+        List[
+            {symbol: ""},
+            {company_name: ""},
+        ]
+    """
+
     permission_classes = [AllowAny]
     serializer_class = StocksSerializer
 
