fab7c211d791bae68bbd8a609506af7ba7369c07
JonasRoo <jonas.roosen@outlook.com>
2020-05-29 22:57:58 +0200

    Registered company model on admin panel

--- backend/stocksbackend/stocks/admin.py
+++ backend/stocksbackend/stocks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Stock, Price
+from .models import Stock, Price, Company
 
 admin.site.register(Stock)
 admin.site.register(Price)
+admin.site.register(Company)
