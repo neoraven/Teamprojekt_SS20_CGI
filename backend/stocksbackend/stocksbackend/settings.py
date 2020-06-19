a78386f8e7bbe24c00d5f8bf2a0aef116a1fd3cb
JonasRoo <jonas.roosen@outlook.com>
2020-06-19 16:08:50 +0200

    Authentication was wrong

--- backend/stocksbackend/stocksbackend/settings.py
+++ backend/stocksbackend/stocksbackend/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
         # "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
     ),
     "EXCEPTION_HANDLER": "stocksbackend.exceptions.exception_handler",
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
