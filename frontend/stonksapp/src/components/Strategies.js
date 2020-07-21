c4fb0dd331f14edeb80c915e01074554f0b34c7f
TobiasV-byte <t.volze@gmail.com>
2020-07-21 19:30:58 +0200

    collapsable view in strategies

--- frontend/stonksapp/src/components/Strategies.js
+++ frontend/stonksapp/src/components/Strategies.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Collapse } from 'antd';
 
+
+const { Panel } = Collapse;
+
+function callback(key) {
+    console.log(key);
+}
 class Strategies extends React.Component {
     render() {
         return (
-            <div>
-
-
-            </div>
+            <Collapse defaultActiveKey={['1']} onChange={callback}>
+                <Panel header="Dogs of the Stocks" key="1">
+                    <p>The 'Dogs of the Stocks' Strategy is an adaptation of the well known 'Dogs of the Dow' Strategy </p>
+                
+                </Panel>
+                <Panel header="This is panel header 2" key="2">
+                    <p>can i has cheezburger?</p>
+                </Panel>
+                <Panel header="This is panel header 3" key="3" disabled>
+                    <p></p>
+                </Panel>
+            </Collapse>
         )
     }
 }
