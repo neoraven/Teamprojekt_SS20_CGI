a23fdea253dc50414f15f0b28551d39cbd55db9b
TobiasV-byte <t.volze@gmail.com>
2020-07-21 13:39:12 +0200

    sliders prototype

--- frontend/stonksapp/src/containers/RecommendationView.js
+++ frontend/stonksapp/src/containers/RecommendationView.js
@@ -4,8 +4,25 @@ import Recommendation from '../components/Recommendations'
 class RecommendationView extends React.Component {
     render() {
         return (
-            <Recommendation/>
+            <div>
+                <h1>Welcome to our simulation-based recommendation system.</h1>
+                <h2>Our algorithm provides you with stock recommendations based on your preferences and an investing strategy of your choices.</h2>
+                <p>What can you tell us about your preferences?</p>
+                <div>
+                    <div>
+                        <p>Risk aversity slider</p>
+                        <Recommendation />
+                        <p>I am risk averse. Therefore, I am an investor who prioritizes the preservation of capital over the potential for a high return.</p>
+                    </div>
+                    <div>
+                        <p>Diversification slider</p>
+                        <Recommendation />
+                        <p>I prefer a diversified portfolio over a concentrated one.</p>
+                    </div>
+                    <Recommendation />
+                </div>
+            </div>
         )
-    } 
+    }
 }
 export default RecommendationView
