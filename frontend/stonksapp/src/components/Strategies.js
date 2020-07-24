060f84ce9dce50fdc9a5f56f615c7375cacb4e81
Tobias Volze <t.volze@gmail.com>
2020-07-24 21:59:49 +0200

    formatting

--- frontend/stonksapp/src/components/Strategies.js
+++ frontend/stonksapp/src/components/Strategies.js
@@ -4,15 +4,28 @@ import { Collapse } from 'antd';
 
 const { Panel } = Collapse;
 
+
+
+
 function callback(key) {
     console.log(key);
 }
 class Strategies extends React.Component {
     render() {
         return (
-            <Collapse defaultActiveKey={['1']} onChange={callback}>
+            <Collapse defaultActiveKey={['1']} onChange={callback} style={{textAlign:"justify"}}>
                 <Panel header="Dogs of the Stocks" key="1">
-                    <p>The 'Dogs of the Stocks' Strategy is an adaptation of the well known 'Dogs of the Dow' Strategy. </p>
+                    <p>
+                    The 'Dogs of the Stocks' Strategy is an adaptation of the well known 'Dogs of the Dow' Strategy.
+                    Under this model, an investor annually reinvesting in high-yield companies has the potential to out-perform the overall market.
+                    The data from Dogs of the Dow suggests that this has been the case since the turn of the century.
+                    The logic behind this is that a high-dividend yield suggests both that the stock is oversold and that management
+                     believes in its company's prospects and is willing to back that up by paying out a relatively high dividend. 
+                     Investors are thereby hoping to benefit from both above-average stock-price gains as well as a relatively high quarterly dividend.
+                     </p>
+                     
+                        
+                     
                 
                 </Panel>
                 <Panel header="This is panel header 2" key="2">
