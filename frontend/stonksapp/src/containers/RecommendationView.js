1e0d8073e830094ad0aed0f147703a4041654e6c
Tobias Volze <t.volze@gmail.com>
2020-07-24 20:18:00 +0200

    added css for RecView

--- frontend/stonksapp/src/containers/RecommendationView.js
+++ frontend/stonksapp/src/containers/RecommendationView.js
@@ -5,7 +5,7 @@ import { Steps, Button, message } from 'antd';
 import { Link } from 'react-router-dom';
 import Strategies from '../components/Strategies';
 import Recommendations from '../components/Recommendations';
-
+import './RecommendationView.css';
 
 const { Step } = Steps;
 const steps = [
@@ -52,11 +52,13 @@ class RecommendationView extends React.Component {
 
 
                         <div>
-                            <Steps current={current}>
-                                {steps.map(item => (
-                                    <Step key={item.title} title={item.title} />
-                                ))}
-                            </Steps>
+                            <div className="steps">
+                                <Steps current={current}>
+                                    {steps.map(item => (
+                                        <Step key={item.title} title={item.title} />
+                                    ))}
+                                </Steps>
+                            </div>
                             <div className="steps-content">{steps[current].content}</div>
                             <div className="steps-action">
                                 {current < steps.length - 1 && (
@@ -85,38 +87,6 @@ class RecommendationView extends React.Component {
 
             </>
         );
-
-
-        /*return (
-    <div>
-        <h1>Welcome to our simulation-based recommendation system.</h1>
-        <h2>Our algorithm provides you with stock recommendations based on your preferences and an investing strategy of your choices.</h2>
-        <p>What can you tell us about your preferences?</p>
-        <Steps current={1}>
-            <Step title="Preferences" description="This is a description." />
-            <Step title="Strategies" description="This is a description." />
-            <Step title="Recommendations" description="This is a description." />
-        </Steps>
-        <div>
-            <div>
-                <p>Risk aversity slider</p>
-                <Recommendation />
-                <p>I am risk averse. Therefore, I am an investor who prioritizes the preservation of capital over the potential for a high return.</p>
-            </div>
-            <div>
-                <p>Diversification slider</p>
-                <Recommendation />
-                <p>I prefer a diversified portfolio over a concentrated one.</p>
-            </div>
-            <div>
-                <p>Diversification slider</p>
-                <Recommendation />
-                <p>I prefer a diversified portfolio over a concentrated one.</p>
-            </div>
-        </div>
-    <Button ><Link to='/chose'>Next</Link></Button>
-    </div>
-)*/
     }
 }
 export default RecommendationView
