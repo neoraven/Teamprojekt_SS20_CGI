0fd38626424351432951b2fcfcdf9dd61badd759
Tobias Volze <t.volze@gmail.com>
2020-08-04 11:02:14 +0200

    stocks/ stockslist now use the batch endpoint in a decent way. prices are being fetched whenever a new page is opened. 10 prices at a time

--- frontend/stonksapp/src/components/Stocks.js
+++ frontend/stonksapp/src/components/Stocks.js
@@ -4,7 +4,6 @@ import Price from './Price';
 import api from '../utils/api';
 
 const { Text } = Typography;
-let once = 0;
 
 class Stocks extends React.Component {
   constructor(props) {
@@ -21,16 +20,16 @@ class Stocks extends React.Component {
 
     }
   }
-  fthis(firstten) {
+  loadNextPrice(firstten) {
     api.get(`/api/stocks/${this.state.symbollist}/prices/most-recent/?batch=true`)
       .then(res => {
         this.setState({
           mostrecentbatchprices: res.data
         })
-      }).then(() => this.backup(firstten))
+      }).then(() => this.combinePricesIntoArray(firstten))
   }
 
-  backup(firstten) {
+  combinePricesIntoArray(firstten) {
     let temp = []
     console.log(this.state.batchprices)
 
@@ -80,58 +79,19 @@ class Stocks extends React.Component {
       this.state.symbollist.push(stock.symbol)
     }
 
-    let fullstocks = this.state.stocks
     console.log(firstten)
     api.get(`/api/stocks/${this.state.symbollist}/prices/most-recent/?batch=true&interval=1d`)
       .then(res => {
         this.setState({
           batchprices: res.data
         })
-      }).then(() => this.fthis(firstten))
-
-
-
-
+      }).then(() => this.loadNextPrice(firstten))
   }
 
   componentDidMount() {
-    console.log("-------------------------------------------------------------")
-    console.log(this.props.data)
-    console.log("-------------------------------------------------------------")
     this.setState({
       stocks: this.props.data
     }, () => this.loadPrices(1))
-    /*if(this.state.stocks[0]!= undefined){
-      this.loadPrices(1)
-    }
-    /* //WORK IN PROGRESS
-
-
-    let firstten = this.state.stocks.slice(0,10)
-
-    api.get(`/api/stocks/${firstten}/prices/most-recent/?batch=true&interval=1d`)
-      .then(res => {
-        this.setState({
-          batchprices: res.data
-        })
-      })
-    api.get(`/api/stocks/${firstten}/prices/most-recent/?batch=true`)
-      .then(res => {
-        this.setState({
-          mostrecentbatchprices: res.data
-        })
-      })
-
-    for(let stock of stocks){
-      for(let batchprice of this.state.batchprices){
-        if(stock.symbol === batchprice.symbol){
-          stock.latestPrice = batchprice.p_close
-        }
-      }
-    }
-    this.setState({
-      stocks: stocks
-    })*/
   }
 
   handleChange = (e) => {
@@ -148,45 +108,6 @@ class Stocks extends React.Component {
         return displayname.toLowerCase().match(searchString);
       })
     }
-    //*************Begin: Hacky implementation of Batch endpoint****************/
-    /* var stocks = this.state.stocks
-        for (let stock of stocks) {
-          this.state.symbollist.push(stock.symbol)
-        }
-        
-    
-        if (once < 2) {
-          api.get(`/api/stocks/${this.state.symbollist}/prices/most-recent/?batch=true&interval=1d`)
-            .then(res => {
-              this.setState({
-                batchprices: res.data
-              })
-            })
-          api.get(`/api/stocks/${this.state.symbollist}/prices/most-recent/?batch=true`)
-            .then(res => {
-              this.setState({
-                mostrecentbatchprices: res.data
-              })
-            })
-          once++;
-        }
-    
-        for (let batchprice of this.state.batchprices) {
-          for (let stock of stocks) {
-          if (batchprice.symbol == stock.symbol) {
-            stock.latestDailyPrice = batchprice.p_close
-            stock.lastUpdated = batchprice.date
-          }
-          }
-        }
-        for (let mostrecentbatchprice of this.state.mostrecentbatchprices) {
-          for (let stock of stocks) {
-          if (mostrecentbatchprice.symbol == stock.symbol) {
-            stock.most_recent = mostrecentbatchprice.p_close
-          }
-          }
-        }*/
-    //*************End: Hacky implementation of Batch endpoint****************/
     if (this.state.stocks[0] != undefined) {
       return (
         <div>
@@ -202,10 +123,11 @@ class Stocks extends React.Component {
             pagination={{
               onChange: page => {
                 console.log(page);
+                this.loadPrices(page)
               },
               pageSize: 10,
             }}
-            dataSource={this.state.stocks} //-> Maybe make stocks its own component to maybe make the prices comps work
+            dataSource={this.state.stocks}
             footer={
               <div>
                 <b></b>
@@ -234,7 +156,7 @@ class Stocks extends React.Component {
         </div>
       )
     } else {
-      return (<div></div>)
+      return (<div>No Stocks could be loaded</div>)
     }
   }
 }
